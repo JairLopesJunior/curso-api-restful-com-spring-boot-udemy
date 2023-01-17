3397426b5e0036ea052b226cefb38251af4cc323
unknown <jair.lopes@fatec.sp.gov.br>
2023-01-17 14:11:21 -0300

    @PathVariable

--- carros/src/main/java/com/example/carros/api/IndexController.java
+++ carros/src/main/java/com/example/carros/api/IndexController.java
@@ -11,8 +11,23 @@ public class IndexController {
         return "Get Spring Boot";
     }
 
-    @GetMapping("/login")
-    public String get(@RequestParam("login") String login, @RequestParam("senha") String senha) {
+//    @GetMapping("/login")
+//    public String get(@RequestParam("login") String login, @RequestParam("senha") String senha) {
+//        return "Login: " + login + ", senha: " + senha;
+//    }
+
+    @GetMapping("/login/{login}/senha/{senha}")
+    public String get(@PathVariable("login") String login, @PathVariable("senha") String senha) {
         return "Login: " + login + ", senha: " + senha;
+    }
+
+    @GetMapping("/carros/{id}")
+    public String getCarroById(@PathVariable("id") Long id) {
+        return "Carro " + id;
+    }
+
+    @GetMapping("/carros/tipo/{tipo}")
+    public String getCarroByTipo(@PathVariable("tipo") String tipo) {
+        return "Lista de Carros " + tipo;
     }
 }

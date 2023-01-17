af9dc6a5ec2373e9f1efc2e12a31030722cbfe8a
unknown <jair.lopes@fatec.sp.gov.br>
2023-01-17 16:33:47 -0300

    @Id @GeneratedValue (Auto vs Identity)

--- carros/src/main/java/com/example/carros/domain/CarroService.java
+++ carros/src/main/java/com/example/carros/domain/CarroService.java
@@ -1,5 +1,6 @@
 package com.example.carros.domain;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -8,7 +9,14 @@ import java.util.List;
 @Service
 public class CarroService {
 
-    public List<Carro> getCarros() {
+    @Autowired
+    private CarroRepository rep;
+
+    public Iterable<Carro> getCarros() {
+        return rep.findAll();
+    }
+
+    public List<Carro> getCarrosFake() {
         return Arrays.asList(
             new Carro(1L, "Fusca"),
             new Carro(2L, "Brasilia"),

1d8cd161b7345ce84659359551c03c309fa1a1cb
unknown <jair.lopes@fatec.sp.gov.br>
2023-01-18 15:10:55 -0300

    O projeto Lombok

--- carros/src/main/java/com/example/carros/domain/Carro.java
+++ carros/src/main/java/com/example/carros/domain/Carro.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Carro {
 
     @Id
@@ -13,35 +19,4 @@ public class Carro {
     private Long id;
     private String nome;
     private String tipo;
-
-    public Carro() {}
-
-    public Carro(Long id, String nome) {
-        this.id = id;
-        this.nome = nome;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
 }

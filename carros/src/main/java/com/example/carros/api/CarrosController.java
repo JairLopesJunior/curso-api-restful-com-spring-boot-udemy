2fd1df2ae76cb1b1a11b73dd999ccb9768eb1ea8
unknown <jair.lopes@fatec.sp.gov.br>
2023-01-18 13:25:40 -0300

    ResponseEntity

--- carros/src/main/java/com/example/carros/api/CarrosController.java
+++ carros/src/main/java/com/example/carros/api/CarrosController.java
@@ -3,6 +3,8 @@ package com.example.carros.api;
 import com.example.carros.domain.Carro;
 import com.example.carros.domain.CarroService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -15,8 +17,8 @@ public class CarrosController {
     private CarroService service;
 
     @GetMapping
-    public Iterable<Carro> get() {
-        return service.getCarros();
+    public ResponseEntity<Iterable<Carro>> get() {
+        return ResponseEntity.ok(service.getCarros());
     }
 
     @GetMapping("/{id}")

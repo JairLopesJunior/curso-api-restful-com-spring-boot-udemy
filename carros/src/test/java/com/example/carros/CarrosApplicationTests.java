2f4d57faa64b509a453049ffd4db31cc1baa89dd
unknown <jair.lopes@fatec.sp.gov.br>
2023-01-19 09:53:30 -0300

    Teste para inserir e Deletar um Carro

--- carros/src/test/java/com/example/carros/CarrosApplicationTests.java
+++ carros/src/test/java/com/example/carros/CarrosApplicationTests.java
@@ -2,9 +2,13 @@ package com.example.carros;
 
 import com.example.carros.domain.Carro;
 import com.example.carros.domain.CarroService;
+import com.example.carros.domain.dto.CarroDTO;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 @SpringBootTest
 class CarrosApplicationTests {
@@ -18,7 +22,26 @@ class CarrosApplicationTests {
 		c.setNome("Ferrari");
 		c.setTipo("esportivos");
 
-		this.service.save(c);
+		CarroDTO carro = this.service.save(c);
+
+		Assertions.assertNotNull(carro);
+
+		Long id = c.getId();
+		Assertions.assertNotNull(id);
+
+		// Buscar o Objeto
+		Optional<CarroDTO> op = service.getCarroById(id);
+		Assertions.assertTrue(op.isPresent());
+
+		carro = op.get();
+		Assertions.assertEquals("Ferrari", carro.getNome());
+		Assertions.assertEquals("esportivos", carro.getTipo());
+
+		// Deletar o objeto
+		service.delete(id);
+
+		// Verificar se deletou
+		Assertions.assertFalse(service.getCarroById(id).isPresent());
 	}
 
 }

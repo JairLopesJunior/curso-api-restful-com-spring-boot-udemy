3599debbbaa18f2ad5819bfa634d9dbb08a038cd
unknown <jair.lopes@fatec.sp.gov.br>
2023-01-19 13:34:25 -0300

    Preenchendo o H2 com uma massa de testes

--- carros/src/main/java/com/example/carros/domain/Carro.java
+++ carros/src/main/java/com/example/carros/domain/Carro.java
@@ -8,15 +8,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
+@Entity
 public class Carro {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
     private String tipo;
+    private String descricao;
+    private String urlFoto;
+    private String urlVideo;
+    private String latitude;
+    private String longitude;
 }
--- carros/src/test/java/com/example/carros/CarrosApplicationTests.java
+++ carros/src/test/java/com/example/carros/CarrosApplicationTests.java
@@ -5,12 +5,16 @@ import com.example.carros.domain.CarroService;
 import com.example.carros.domain.dto.CarroDTO;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 import java.util.Optional;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class CarrosApplicationTests {
 
@@ -50,7 +54,26 @@ class CarrosApplicationTests {
 
 		List<CarroDTO> carros = service.getCarros();
 
-		Assertions.assertEquals(31, carros.size());
+		Assertions.assertEquals(30, carros.size());
+	}
+
+	@Test
+	public void testGet() {
+
+		Optional<CarroDTO> op = service.getCarroById(11L);
+
+		Assertions.assertTrue(op.isPresent());
+
+		CarroDTO carroDTO = op.get();
+
+		Assertions.assertEquals("Ferrari FF", carroDTO.getNome());
+	}
+
+	@Test
+	public void testListaPorTipo() {
+		Assertions.assertEquals(10, service.getCarrosByTipo("classicos").size());
+		Assertions.assertEquals(10, service.getCarrosByTipo("esportivos").size());
+		Assertions.assertEquals(10, service.getCarrosByTipo("luxo").size());
 	}
 }
 

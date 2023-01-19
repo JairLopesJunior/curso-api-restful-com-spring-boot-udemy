1c3ea9941b75e768d8486a0aa38194d0b768efd3
unknown <jair.lopes@fatec.sp.gov.br>
2023-01-19 09:25:32 -0300

    Teste para Inserir um Carro

--- carros/src/test/java/com/example/carros/CarrosApplicationTests.java
+++ carros/src/test/java/com/example/carros/CarrosApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.carros;
 
+import com.example.carros.domain.Carro;
+import com.example.carros.domain.CarroService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CarrosApplicationTests {
 
+	@Autowired
+	private CarroService service;
+
 	@Test
-	void contextLoads() {
+	void test1() {
+		Carro c = new Carro();
+		c.setNome("Ferrari");
+		c.setTipo("esportivos");
+
+		this.service.save(c);
 	}
 
 }
